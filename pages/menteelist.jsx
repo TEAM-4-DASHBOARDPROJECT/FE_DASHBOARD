741782dbe3f70aed98e68297358eb40d1e192a92
Yoga Sulistyo <kgsyoga1997@gmail.com>
2022-09-20 23:35:30 +0700

    revisi modal

---
+++ pages/menteelist.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import Sidebar from "../component/sidebar";
+import { Form, Modal, Button, Container, Row, Col } from "react-bootstrap";
+
+function AddNew(props) {
+  return (
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Add New Mentee</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="show-grid">
+        <Container>
+          <Row>
+            <Col xs={12} md={12}>
+              <label className="me-5 mb-5">Full Name</label>
+              <input placeholder="Nama"></input>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12}>
+              <Form.Select aria-label="Default select example">
+                <option>Status</option>
+                <option value="1">Active</option>
+                <option value="2">Repeat</option>
+                <option value="3">Dropout</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12}>
+              <label className="me-5 mb-5 mt-5">Class</label>
+              <input placeholder="Class"></input>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12}>
+              <Form.Select aria-label="Default select example">
+                <option>Category</option>
+                <option value="1">Informatic</option>
+                <option value="2">Non-Informatic</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Row className="mb-5">
+            <Col xs={12} md={12}>
+              <Form.Select aria-label="Default select example">
+                <option>Gender</option>
+                <option value="1">Male</option>
+                <option value="2">Female</option>
+              </Form.Select>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>save</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+const menteelist = () => {
+  const [modalShowNew, setModalNew] = useState(false);
+  return (
+    <div className="main-container d-flex">
+      <Sidebar />
+      <div className="container">
+        <div className="menteelog d-flex justify-content-around">
+          <div>
+            <h1>Immersive Dashboard</h1>
+            <h2>Class List</h2>
+          </div>
+
+          <div className="hello">
+            Hello, <span className="name">Kamil</span>
+          </div>
+        </div>
+        <div>
+          <div className="searchbutton">
+            <input type="search"></input>
+            <button>Search</button>
+            <button onClick={() => setModalNew(true)}>Add New</button>
+            <AddNew show={modalShowNew} onHide={() => setModalNew(false)} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default menteelist;
