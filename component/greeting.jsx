391652d72f618c1251e0588d61d468ea6c6a654f
doffaarinalhaq <adoffa@gmail.com>
2022-09-21 09:54:33 +0700

    dashboard

---
+++ component/greeting.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Greeting = () => {
+  return (
+    <div className="congreet">
+      <Row>
+        <Col md={{ span: 8, offset: 0 }} className='colgreet1'>
+          <div>
+            <h1>Immerive Dashboard</h1>
+            <h2>Dashboard</h2>
+          </div>
+        </Col>
+        <Col md={{ span: 4, offset: 0 }}>
+          <div className="hellogreet">
+            Hello, <span className="name">Kamil</span>
+          </div>
+        </Col>
+      </Row>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default Greeting;
