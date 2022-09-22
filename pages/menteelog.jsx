5b6208242b6d4ff42e30f2cdc638c7bdc7f734f5
syaanwar26 <sya.anwar26@gmail.com>
2022-09-23 01:39:41 +0700

    revisi 1 api log

--- pages/menteelog.jsx
+++ pages/menteelog.jsx
@@ -3,9 +3,45 @@ import Sidebar from '../component/sidebar';
 import Greeting from '../component/greeting';
 import { Row, Col, Modal, Container, Button, Form } from 'react-bootstrap';
 import { AiOutlineFileSearch } from 'react-icons/ai';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 
 function AddNewLog(props) {
+  const [status, setStatus] = useState("");
+  const [date, setDate] = useState("");
+  const [feed, setFeed] = useState("");
+  const [file, setFile] = useState("");
+
+  const handleNewLog = () => {
+    var axios = require("axios");
+    var data = JSON.stringify({
+      status: status,
+      date: date,
+      feedback: feed,
+      file: file,
+    });
+
+    var config = {
+      method: "post",
+      url: "https://group4.altaproject.online/manager",
+      headers: {
+        Authorization: `Bearer ${getCookie("Token")}`,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(response)
+        alert("data sudah masuk yaa");
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Data gagal masuk nih");
+      })
+  }
+
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
       <Modal.Header closeButton>
@@ -17,9 +53,9 @@ function AddNewLog(props) {
             <Col xs={12} md={8}>
               <Form.Select aria-label="Default select example">
                 <option>Status</option>
-                <option value="1">Active</option>
-                <option value="2">Repeat</option>
-                <option value="3">Dropout</option>
+                <option value="active">Active</option>
+                <option value="repeat">Repeat</option>
+                <option value="dropout">Dropout</option>
               </Form.Select>
             </Col>
             <Col xs={6} md={4}>
@@ -51,6 +87,9 @@ function AddNewLog(props) {
 const Menteelog = () => {
   const [modalShow, setModalShow] = useState(false);
 
+  const router = useRouter();
+  const datas = router.query;
+
   const logOut = () => {
     Router.push({
       pathname: '/',
@@ -71,16 +110,15 @@ const Menteelog = () => {
             <div>
               <Row>
                 <Col sm={12} md={6}>
-                  <h1 className="fs-3 pt-0">Rachman Kamil</h1>
-                  <h2 className="fs-6">Quality Enggineer Batch 8</h2>
-                  <h2 className="fs-6">IPA</h2>
-                  <h2 className="fs-6">SMA N 29 Palembang</h2>
+                  <h1 className="fs-3 pt-0">{datas.name}</h1>
+                  <h2 className="fs-6">{datas.class_id}</h2>
+                  <h2 className="fs-6">{datas.educationmajor}</h2>
+                  <h2 className="fs-6">{datas.educationgraduate}</h2>
                 </Col>
                 <Col sm={12} md={6}>
-                  <h2 className="fs-6">Phone : 08218341294</h2>
-                  <h2 className="fs-6">Telegram:@Rachman</h2>
-                  <h2 className="fs-6">Discord:@Rachman</h2>
-                  <h2 className="fs-6">Email:Rachman@gmail.com</h2>
+                  <h2 className="fs-6">Phone : {datas.phone}</h2>
+                  <h2 className="fs-6">Telegram : {datas.telegram}</h2>
+                  <h2 className="fs-6">{datas.email}</h2>
                 </Col>
               </Row>
             </div>
@@ -95,7 +133,6 @@ const Menteelog = () => {
                 <Col xs={3}>
                   <div>
                     <p>22 Oktober 2022</p>
-                    <p>End Of Section</p>
                     <p className="fs-1">
                       <AiOutlineFileSearch />
                     </p>
