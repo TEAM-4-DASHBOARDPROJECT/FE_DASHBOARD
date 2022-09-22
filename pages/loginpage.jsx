575801adfe40f1faddf005f58e8a3244c5367800
doffaarinalhaq <adoffa@gmail.com>
2022-09-22 16:05:24 +0700

    login uodate

--- pages/loginpage.jsx
+++ pages/loginpage.jsx
@@ -1,34 +1,34 @@
-import Image from "next/image";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import Link from "next/link";
-import img from "../picture/login.png";
-import { useState } from "react";
-import Router from "next/router";
+import Image from 'next/image';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import Link from 'next/link';
+import img from '../picture/login.png';
+import { useState } from 'react';
+import Router from 'next/router';
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    var axios = require("axios");
+    var axios = require('axios');
     var data = JSON.stringify({
       email: email,
       password: password,
     });
 
     var config = {
-      method: "post",
-      url: "http://3.86.24.153:80/login",
+      method: 'post',
+      url: 'http://3.86.24.153:80/login',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       data: data,
     };
 
     axios(config)
       .then(function (response) {
-        alert("login berhasil");
-        Router.push("/dashboard");
+        alert('login berhasil');
+        Router.push('/dashboard');
         console.log(JSON.stringify(response.data));
       })
       .catch(function (error) {
@@ -37,47 +37,53 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="log-page">
-      <Image src={img} width="600px"></Image>
-      <Container style={{ width: "600px", height: "1096" }}>
-        <div>
-          <h2>Login to your account!</h2>
-        </div>
-        <Form>
-          <Row>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Col>
-                <Form.Label>Email</Form.Label>
-              </Col>
-              <Col>
-                <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
-              </Col>
-              <Col>
-                <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-              </Col>
-            </Form.Group>
-          </Row>
-          <Row>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Col>
-                <Form.Label>Password</Form.Label>
-              </Col>
-              <Col>
-                <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
-              </Col>
-            </Form.Group>
-          </Row>
-          <Row style={{ float: "right" }}>
-            <Form.Group>
-              <Link href={"/resetpassword"}>Reset Password</Link>
-            </Form.Group>
-          </Row>
-        </Form>
-        <div>
-          <Button onClick={handleLogin} className="button" style={{ background: "#F07539", border: "#f7731c", width: "209px", marginTop: "35px"}}>
-            LOGIN
-          </Button>
-        </div>
+    <div>
+      <Container className="contlog">
+        <Row className="contlog">
+          <Col md={{ span: 7 }} className="contlog">
+            <Image src={img} alt="Picture of the author" width="550px" height="625px" />
+          </Col>
+          <Col md={{ span: 5 }} style={{ paddingTop: '150px !important' }} className="contlog">
+            <div>
+              <h2>Login to your account!</h2>
+            </div>
+            <Form>
+              <Row>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Col>
+                    <Form.Label>Email</Form.Label>
+                  </Col>
+                  <Col>
+                    <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+                  </Col>
+                  <Col>
+                    <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                  </Col>
+                </Form.Group>
+              </Row>
+              <Row>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Col>
+                    <Form.Label>Password</Form.Label>
+                  </Col>
+                  <Col>
+                    <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+                  </Col>
+                </Form.Group>
+              </Row>
+              <Row style={{ float: 'right' }}>
+                <Form.Group>
+                  <Link href={'/resetpassword'}>Reset Password</Link>
+                </Form.Group>
+              </Row>
+            </Form>
+            <div>
+              <Button onClick={handleLogin} className="buttonlogin" style={{ background: '#F07539', border: '#f7731c', width: '150px', height: '35px', marginTop: '35px' }}>
+                <p>LOGIN</p>
+              </Button>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
