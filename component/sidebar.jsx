62d54c5fe9e9a50cf564a25aa0a6e1e7d42d4069
doffaarinalhaq <adoffa@gmail.com>
2022-09-22 16:27:29 +0700

    update db

--- component/sidebar.jsx
+++ component/sidebar.jsx
@@ -1,29 +1,31 @@
-import React from "react";
-import Image from "next/image";
-import logo from "../picture/logoputih.png";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import Image from 'next/image';
+import logo from '../picture/logoputih.png';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <nav className="sidebar navbar navbar-expand d-flex flex-column alignt-item-start" id="side_nav">
-      <div className="logosb">
-        <Image src={logo} alt="Picture of the author" width="237px" height="122px" />
-      </div>
-      <div className="isisidebar">
-        <div className="menudash">
-          <a href="/dashboard">Dashboard</a>
+    <div className='contsb'>
+      <nav className="sidebar navbar navbar-expand d-flex flex-column alignt-item-start" id="side_nav">
+        <div className="logosb">
+          <Image src={logo} alt="Picture of the author" width="237px" height="122px" />
         </div>
-        <div className="menu">
-          <a href="/menteelist">Mentee</a>
+        <div className="isisidebar">
+          <div className="menudash">
+            <a href="/dashboard">Dashboard</a>
+          </div>
+          <div className="menu">
+            <a href="/menteelist">Mentee</a>
+          </div>
+          <div className="menuser">
+            <a href="/userlist">User</a>
+          </div>
+          <div className="menu">
+            <a href="/class">Class</a>
+          </div>
         </div>
-        <div className="menuser">
-          <a href="/userlist">User</a>
-        </div>
-        <div className="menu">
-          <a href="/class">Class</a>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Sidebar from "../component/sidebar";
-import Greeting from "../component/greeting";
-import Chart from "../component/chart";
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import Sidebar from '../component/sidebar';
+import Greeting from '../component/greeting';
+import Chart from '../component/chart';
+import { Container, Row, Col } from 'react-bootstrap';
 import Router from 'next/router';
 
 const dashboard = () => {
@@ -14,33 +14,35 @@ const dashboard = () => {
 
   return (
     <div className="condash">
-      <Row>
-        <Col lg={{ span: 4, offset: 0 }} className="col1">
-          <Sidebar />
-        </Col>
-        <Col lg={{ span: 8, offset: 0 }} className="col2">
-          <div className="colr2">
-            <Greeting title="Dashboard" clickLogOut={() => logOut()} />
-          </div>
-          <div className="div0">
-            <div className="div1">
-              <h1>52</h1>
-              <h5>Mentee Active</h5>
+      <Container className="condash1">
+        <Row>
+          <Col lg={{ span: 4, offset: 0 }} className="col1">
+            <Sidebar />
+          </Col>
+          <Col lg={{ span: 8, offset: 0 }} className="col2">
+            <div className="colr2">
+              <Greeting title="Dashboard" clickLogOut={() => logOut()} />
             </div>
-            <div className="div2">
-              <h1>52</h1>
-              <h5>Mentee Placement</h5>
+            <div className="div0">
+              <div className="div1">
+                <h1>52</h1>
+                <h5>Mentee Active</h5>
+              </div>
+              <div className="div2">
+                <h1>52</h1>
+                <h5>Mentee Placement</h5>
+              </div>
+              <div className="div3">
+                <h1>52</h1>
+                <h5>Mentee Feedback</h5>
+              </div>
+              <div className="div4">
+                <Chart />
+              </div>
             </div>
-            <div className="div3">
-              <h1>52</h1>
-              <h5>Mentee Feedback</h5>
-            </div>
-            <div className="div4">
-              <Chart />
-            </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
